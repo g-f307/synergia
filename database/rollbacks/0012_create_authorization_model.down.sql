de228aff9bd698b361a38b2efffa40c39f586a4a
NunesGustavo0 <gustavo.nunes0379@gmail.com>
2026-08-31 15:38:27 -0400

    feat(authorization): modelar grupos papéis e permissões

---
+++ database/rollbacks/0012_create_authorization_model.down.sql
@@ -0,0 +1,7 @@
+DROP TABLE synergia.role_permissions;
+DROP TABLE synergia.user_role_assignments;
+DROP TABLE synergia.user_group_memberships;
+DROP TABLE synergia.permissions;
+DROP TABLE synergia.roles;
+DROP TABLE synergia.identity_groups;
+DROP TABLE synergia.iam_organizations;
